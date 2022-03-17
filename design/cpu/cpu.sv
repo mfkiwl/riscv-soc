efde5e921d99d86cc56a5034de62f303184c106b
Dominik Salvet <dominik.salvet@gmail.com>
2022-03-17 21:28:38 +0100

    add initial CPU source files

---
+++ design/cpu/cpu.sv
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// Copyright 2022 Dominik Salvet
+// https://github.com/dominiksalvet/riscv-soc
+//------------------------------------------------------------------------------
+
+module cpu (
+    input rst_l,
+    input clk
+);
+    
+endmodule
